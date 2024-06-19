4aeb03b4662ebdfcf529856f71886b1c6e271137
Arsh Goyal <arshgoyalDev@gmail.com>
2024-06-19 16:40:52 +0530

    added custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          purple: "hsl(259, 100%, 65%)",
+          lightRed: "hsl(0, 100%, 67%)",
+        },
+        neutral: {
+          offWhite: "hsl(0, 0%, 94%)",
+          lightGrey: "hsl(0, 0%, 86%)",
+          smokeyGray: "hsl(0, 1%, 44%)",
+          offBlack: "hsl(0, 0%, 8%)",
+        },
+      },
+      borderRadius: {
+        "4xl": "6.5rem",
+      },
+    },
   },
   plugins: [],
-}
+};
